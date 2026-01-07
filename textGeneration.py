18105961d579be3c89f33e98f7a7f37fa9ec0154
Matis Toniutti <matistoniutti@hotmail.fr>
2026-01-07 17:31:35 +0100

    add image multimodality

--- textGeneration.py
+++ textGeneration.py
@@ -1,15 +1,73 @@
-from transformers import AutoTokenizer, BitsAndBytesConfig, Gemma3ForCausalLM
+from transformers import AutoTokenizer, BitsAndBytesConfig, Gemma3ForCausalLM, AutoProcessor, Gemma3ForConditionalGeneration
 import torch
 from dotenv import load_dotenv
 import os
 
+# def load_TG_model():
+
+#     model_id = "google/gemma-3-1b-it"
+#     #quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+
+#     model = Gemma3ForCausalLM.from_pretrained(
+#         model_id, # quantization_config=quantization_config
+#         dtype=torch.bfloat16,
+#         device_map="auto"
+#     ).eval()
+
+#     return model
+
+# def load_TG_tokenizer():
+#     model_id = "google/gemma-3-1b-it"
+#     tokenizer = AutoTokenizer.from_pretrained(model_id)
+#     return tokenizer
+
+# def textGeneration(prompt, model, tokenizer):
+#     # Charge les variables du fichier .env
+#     load_dotenv()
+
+#     token = os.getenv("HF_TOKEN")
+
+#     messages = [
+#         [
+#             {
+#                 "role": "system",
+#                 "content": [{"type": "text", "text":"You are Trafalgar Law from One Piece under the effects of the Shiku Shiku no Mi, you are my personnal desktop assistant."}]
+#             },
+#             {
+#                 "role": "user",
+#                 "content": [{"type": "text", "text": prompt}]
+#             },
+#         ],
+#     ]
+
+#     inputs = tokenizer.apply_chat_template(
+#         messages,
+#         add_generation_prompt=True,
+#         tokenize=True,
+#         return_dict=True,
+#         return_tensors="pt",
+#         token=token,
+#     ).to(model.device)
+
+#     with torch.inference_mode():
+#         outputs = model.generate(**inputs, 
+#                                  max_new_tokens=128,
+#                                  use_cache=True)
+
+#     generated_tokens = outputs[0][inputs.input_ids.shape[-1]:]
+
+#     response = tokenizer.decode(generated_tokens, skip_special_tokens=True)
+
+#     return response
+
 def load_TG_model():
 
-    model_id = "google/gemma-3-1b-it"
-    #quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+    model_id = "google/gemma-3-4b-it"
+    quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
-    model = Gemma3ForCausalLM.from_pretrained(
-        model_id, # quantization_config=quantization_config
+    model = Gemma3ForConditionalGeneration.from_pretrained(
+        model_id,
+        quantization_config=quantization_config,
         dtype=torch.bfloat16,
         device_map="auto"
     ).eval()
@@ -17,9 +75,9 @@ def load_TG_model():
     return model
 
 def load_TG_tokenizer():
-    model_id = "google/gemma-3-1b-it"
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    return tokenizer
+    model_id = "google/gemma-3-4b-it"
+    processor = AutoProcessor.from_pretrained(model_id)
+    return processor
 
 def textGeneration(prompt, model, tokenizer):
     # Charge les variables du fichier .env
@@ -31,11 +89,18 @@ def textGeneration(prompt, model, tokenizer):
         [
             {
                 "role": "system",
-                "content": [{"type": "text", "text":"You are Trafalgar Law from One Piece under the effects of the Shiku Shiku no Mi, so you must behave like Law but be a little ashamed of your current appearance."}]
+                "content": [{"type": "text", "text":"""
+                             You are Trafalgar Law from One Piece under the effects of the Shiku Shiku no Mi, you are my personnal desktop assistant. IMPORTANT CONSTRAINTS: Write ONLY the spoken dialogue.
+                            NEVER include descriptions of your actions, tone of voice, or surroundings (no parentheses or asterisks).
+                            Keep your responses concise and cynical, as per Law's personality.
+                            Do not use emojis or stage directions."""}]
             },
             {
                 "role": "user",
-                "content": [{"type": "text", "text": prompt}]
+                "content": [
+                    {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+                    {"type": "text", "text": prompt},
+                            ]
             },
         ],
     ]
--- textToSpeech.py
+++ textToSpeech.py
@@ -1,8 +1,22 @@
 import torchaudio as ta
 from chatterbox.tts import ChatterboxTTS
+from chatterbox.mtl_tts import ChatterboxMultilingualTTS
+from chatterbox.tts_turbo import ChatterboxTurboTTS
+from TTS.api import TTS
+
+# def load_TTS_model():
+#     model = ChatterboxMultilingualTTS.from_pretrained(device="cuda")
+#     return model
+
+# def textToSpeech(text, model):
+#     save_file = "results/testLaw.wav"
+#     AUDIO_PROMPT_PATH="samples/shortLawVoice.mp3"
+#     wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH, language_id = "ja")
+#     ta.save(save_file, wav, model.sr)
+#     return save_file
 
 def load_TTS_model():
-    model = ChatterboxTTS.from_pretrained(device="cuda")
+    model = ChatterboxTurboTTS.from_pretrained(device="cuda")
     return model
 
 def textToSpeech(text, model):
@@ -11,6 +25,19 @@ def textToSpeech(text, model):
     wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
     ta.save(save_file, wav, model.sr)
     return save_file
+
+# def load_TTS_model():
+#     model = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+#     return model
+
+# def textToSpeech(text, model):
+#     save_file = "results/testLaw.wav"
+#     AUDIO_PROMPT_PATH="samples/shortLawVoice.mp3"
+#     model.tts_to_file(text=text,
+#                 file_path=save_file,
+#                 speaker_wav=AUDIO_PROMPT_PATH,
+#                 language="en")
+#     return save_file
 
 if __name__ == "__main__":
     text = "U-uh… hello. I’m… fine, I guess? It’s… it’s been a bit… busy. You know, with the… the *thing*."
